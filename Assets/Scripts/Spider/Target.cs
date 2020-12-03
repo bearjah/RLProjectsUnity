885157f9622327ee7674a3bdef801a4def3c74ca
bearjah <amit.berman@gmail.com>
2020-12-03 17:04:42 +0200

    added static target instead agent prey, run training of middle spawn target

--- Assets/Scripts/Spider/Target.cs
+++ Assets/Scripts/Spider/Target.cs
@@ -5,30 +5,48 @@ using Random = UnityEngine.Random;
 
 public class Target : MonoBehaviour
 {
-    public SpiderAgent agent;
-    float distanceToAgent;
+    public SpiderAgent spider;
+    private float distanceToSpider;
+    private bool isRandomRespawn = false;
 
+    private void Awake()
+    {
+        isRandomRespawn = false;
+    }
     void OnTriggerEnter(Collider col)
     {
         // Touched target.
         if (col.gameObject.CompareTag("spiderAgent"))
         {
             gameObject.SetActive(false);
-            agent.ReachedTarget();
+            spider.ReachedTarget();
             Respawn();
         }
     }
     public void Respawn()
     {
         this.gameObject.SetActive(true);
-        Vector3 randomPosition;
-        do
+        if(isRandomRespawn)
         {
-            randomPosition = new Vector3(Random.Range(-4.5f, 4.5f) + transform.parent.transform.position.x, 1f, Random.Range(-4.5f, 4.5f) + transform.parent.transform.position.z);
-            distanceToAgent = Vector3.Distance(randomPosition, agent.transform.position);
-        } while (distanceToAgent < 3f);
+            Vector3 randomPosition;
+            do
+            {
+                randomPosition = new Vector3(Random.Range(-4.5f, 4.5f), 1f, Random.Range(-4.5f, 4.5f)) + transform.parent.transform.position;
+                distanceToSpider = Vector3.Distance(randomPosition, spider.transform.position);
+            } while (distanceToSpider < 3f);
 
-        transform.position = randomPosition;
+            transform.position = randomPosition;
+            return;
+        }
+        transform.position = new Vector3(0f, 1f, 0f) + transform.parent.transform.position;
+        // Vector3 randomPosition;
+        // do
+        // {
+        //     randomPosition = new Vector3(Random.Range(-4.5f, 4.5f) + transform.parent.transform.position.x, 1f, Random.Range(-4.5f, 4.5f) + transform.parent.transform.position.z);
+        //     distanceToSpider = Vector3.Distance(randomPosition, agent.transform.position);
+        // } while (distanceToSpider < 3f);
+
+        // transform.position = randomPosition;
     }
 }
 
